a3736298a9b6acbed896e92ec4687e3026a4dba2
OGUNNOWO OLUDARE <damilareogunnowo10@gmail.com>
2025-09-04 15:39:51 +0100

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -91,11 +91,6 @@ DATABASES = {
         default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
         engine='django.db.backends.postgresql',
-        host='containers-us-west-49.railway.app',
-        port='6371',
-        user='postgres',
-        password='oTcUJmUaItYjlxVtyEGioSfdBvMTqjAv',
-        database='railway'
         
     )
 }
