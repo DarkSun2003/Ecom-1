cd41ed0a49032e40f6b108712827950cb7e22d54
OGUNNOWO OLUDARE <damilareogunnowo10@gmail.com>
2025-09-04 16:01:04 +0100

    trials

--- ecom/settings.py
+++ ecom/settings.py
@@ -88,7 +88,6 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
-        engine='django.db.backends.postgresql',
         
     )
 }
