51f5e22a7c835f49823c546f0a0a36669a7d19bf
OGUNNOWO OLUDARE <damilareogunnowo10@gmail.com>
2025-09-06 19:38:00 +0100

    pls work

--- ecom/settings.py
+++ ecom/settings.py
@@ -1,9 +1,7 @@
 """
 Django settings for ecom project.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/topics/settings/
-
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
@@ -21,16 +19,21 @@ load_dotenv()
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key')
 
-# Set DEBUG from environment variable
+# Set DEBUG from environment variable for production
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-# Dynamically set ALLOWED_HOSTS for development and production
+# --- CHANGE MADE HERE ---
+# Dynamically set ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS
 if DEBUG:
-    ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-    CSRF_TRUSTED_ORIGINS = []
+    ALLOWED_HOSTS = ['*']
+    CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8000']
 else:
-    ALLOWED_HOSTS = ['ecom-1-production-59cf.up.railway.app']
-    CSRF_TRUSTED_ORIGINS = ['https://ecom-1-production-59cf.up.railway.app']
+    # Add your Railway production domain here
+    ALLOWED_HOSTS = [os.getenv('RAILWAY_STATIC_URL')]
+    CSRF_TRUSTED_ORIGINS = [f"https://{os.getenv('RAILWAY_STATIC_URL')}"]
+
+# For debugging purposes, print the DEBUG value to your terminal
+print(f"Django DEBUG is set to: {DEBUG}")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -80,7 +83,7 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ['DATABASE_URL'],
+        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
         conn_max_age=600,
     )
 }
@@ -106,14 +109,17 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# Cloudinary settings only for production
-if not DEBUG:
+# Conditional storage for development vs. production
+if DEBUG:
+    # Use local storage in development
+    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+else:
+    # Use Cloudinary storage in production
     CLOUDINARY_STORAGE = {
         'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
         'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
         'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
     }
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- ecom/urls.py
+++ ecom/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import settings
+from django.conf import settings
 from django.conf.urls.static import static
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +26,7 @@ urlpatterns = [
     path('payment/', include('payment.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# This is the new code to serve media files in development
+if settings.DEBUG:
+    print("Serving media files for development...")
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
